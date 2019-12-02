d4598da5cf793544b389dbf1dba35894f42ae73f
Kartik Yendamuri <kartikyendamuri@gmail.com>
2019-12-02 17:17:08 -0500

    test

--- Battleships.py
+++ Battleships.py
@@ -9,7 +9,7 @@ def print_board(board):
     for row in board:
         print((" ").join(row))
 
-print("Let's play Battleship with you!")
+print("Let's play Battleship!")
 print_board(board)
 
 def random_row(board):
